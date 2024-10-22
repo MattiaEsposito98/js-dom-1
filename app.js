9c13f0373a57723408feef2c7dd7ce2f6596ad33
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-22 17:09:42 +0200

    add spento - acceso

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 console.log("Lampadina")
 
 myButton.addEventListener('click', function () {
-  const img = document.getElementById("changeImg")
   const myButton = document.getElementById("myButton")
-  
-  img.src = './img/white_lamp.png'
-  myButton.innerText = "Spegni"
+  const img = document.getElementById ("changeImg")
+
+  if (myButton.textContent === 'Accendi') {
+    img.src = "./img/yellow_lamp.png"
+    myButton.textContent = 'Spegni'
+
+  } else {
+    img.src = "./img/white_lamp.png"
+    myButton.textContent = 'Accendi'
+
+  }
+
 })
 
 
