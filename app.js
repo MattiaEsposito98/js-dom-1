610a456d3b0cd520c5eec0b38b6e2125369cad5c
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-22 16:03:24 +0200

    camcio immagine e scritta bottone

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Lampadina")
+function clickButtonOn() {
+  const img = document.getElementById("changeImg")
+  const myButton = document.getElementById("myButton")
+
+  img.src = './img/yellow_lamp.png'
+  myButton.innerText = "Spegni"
+}
+
+
+
+
+
+
+
+
